7111b28ca6f90246e0eeff5fa4e6dcc6771082a6
jasselin <jason.asselin@cdid.com>
2018-06-29 15:39:35 -0400

    webpack.config.js + test UI

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import _ from 'lodash';
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
 
+function module1Button() {
+    var element = document.createElement('input');
+
+    element.type = 'button';
+    element.name = "btnModule1";
+    element.value = 'Module 1';
+
+    element.addEventListener('click', function() {
+        console.log('module 1');
+    });
+
+    return element;
+}
+
+function module2Button() {
+    var element = document.createElement('input');
+
+    element.type = 'button';
+    element.name = "btnModule2";
+    element.value = 'Module 2';
+
+    element.addEventListener('click', function() {
+        console.log('module 2');
+    });
+
+    return element;
+}
+
 document.body.appendChild(component());
+document.body.appendChild(module1Button());
+document.body.appendChild(module2Button());
